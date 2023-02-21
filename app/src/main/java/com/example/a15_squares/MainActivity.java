430f6d9d5f90cf2a56b8521d1289cbcb3975378e
21hph <hpham69032003@gmail.com>
2023-02-21 02:51:53 -0800

    2D Array Rep

--- app/src/main/java/com/example/a15_squares/MainActivity.java
+++ app/src/main/java/com/example/a15_squares/MainActivity.java
@@ -5,16 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.widget.Button;
+
+import java.util.Locale;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // member variables
-        Random rand = new Random();
-        int randomNum = rand.nextInt(15)+1;
-
         // program create initialization
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
@@ -24,22 +22,93 @@ public class MainActivity extends AppCompatActivity {
         SquaresView squaresView = findViewById(R.id.squares);
         SquaresController squaresController = new SquaresController(squaresView);
 
-        // randomizes numbers on buttons on create
-
+        // member variables
+        int[][] squaresArray = squaresView.squaresArray();
 
         // buttons
         // b11
         Button b11 = findViewById(R.id.b11);
         b11.setOnClickListener(squaresController);
-        b11.setText("" + randomNum);
+        b11.setText(String.format(Locale.US, "%d", squaresArray[0][0]));
+
 
         // b12
-//        Button b12 = findViewById(R.id.b12);
-//        b12.setOnClickListener(squaresController);
-//        b12.setText("" + randomNum);
+        Button b12 = findViewById(R.id.b12);
+        b12.setOnClickListener(squaresController);
+        b12.setText(String.format(Locale.US, "%d", squaresArray[0][1]));
 
-        squaresView.randomizeNumbers();
+        // b13
+        Button b13 = findViewById(R.id.b13);
+        b13.setOnClickListener(squaresController);
+        b13.setText(String.format(Locale.US, "%d", squaresArray[0][2]));
 
+        // b14
+        Button b14 = findViewById(R.id.b14);
+        b14.setOnClickListener(squaresController);
+        b14.setText(String.format(Locale.US, "%d", squaresArray[0][3]));
 
+        // b21
+        Button b21 = findViewById(R.id.b21);
+        b21.setOnClickListener(squaresController);
+        b21.setText(String.format(Locale.US, "%d", squaresArray[1][0]));
+
+        // b22
+        Button b22 = findViewById(R.id.b22);
+        b22.setOnClickListener(squaresController);
+        b22.setText(String.format(Locale.US, "%d", squaresArray[1][1]));
+
+        // b23
+        Button b23 = findViewById(R.id.b23);
+        b23.setOnClickListener(squaresController);
+        b23.setText(String.format(Locale.US, "%d", squaresArray[1][2]));
+
+        // b24
+        Button b24 = findViewById(R.id.b24);
+        b24.setOnClickListener(squaresController);
+        b24.setText(String.format(Locale.US, "%d", squaresArray[1][3]));
+
+        // b31
+        Button b31 = findViewById(R.id.b31);
+        b31.setOnClickListener(squaresController);
+        b31.setText(String.format(Locale.US, "%d", squaresArray[2][0]));
+
+        // b32
+        Button b32 = findViewById(R.id.b32);
+        b32.setOnClickListener(squaresController);
+        b32.setText(String.format(Locale.US, "%d", squaresArray[2][1]));
+
+        // b33
+        Button b33 = findViewById(R.id.b33);
+        b33.setOnClickListener(squaresController);
+        b33.setText(String.format(Locale.US, "%d", squaresArray[2][2]));
+
+        // b34
+        Button b34 = findViewById(R.id.b34);
+        b34.setOnClickListener(squaresController);
+        b34.setText(String.format(Locale.US, "%d", squaresArray[2][3]));
+
+        // b41
+        Button b41 = findViewById(R.id.b41);
+        b41.setOnClickListener(squaresController);
+        b41.setText(String.format(Locale.US, "%d", squaresArray[3][0]));
+
+        // b42
+        Button b42 = findViewById(R.id.b42);
+        b42.setOnClickListener(squaresController);
+        b42.setText(String.format(Locale.US, "%d", squaresArray[3][1]));
+
+        // b43
+        Button b43 = findViewById(R.id.b43);
+        b43.setOnClickListener(squaresController);
+        b43.setText(String.format(Locale.US, "%d", squaresArray[3][2]));
+
+        // b44
+        Button b44 = findViewById(R.id.b44);
+        b44.setOnClickListener(squaresController);
+        b44.setText(String.format(Locale.US, "%d", squaresArray[3][3]));
+
+        // reset button
+        Button reset = findViewById(R.id.reset);
+        reset.setOnClickListener(squaresController);
     }
 }
--- app/src/main/java/com/example/a15_squares/SquaresView.java
+++ app/src/main/java/com/example/a15_squares/SquaresView.java
@@ -2,9 +2,11 @@ package com.example.a15_squares;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class SquaresView extends TableLayout {
@@ -15,14 +17,47 @@ public class SquaresView extends TableLayout {
 
     }
 
-    public void randomizeNumbers() {
+    // Method that randomizes the numbers in an array
+    public int[] randomizeNumbers() {
         Random rand = new Random();
-        int[] randomInts = new int[15];
+        int[] randomInts = new int[16];
 
-        // randomizes array of ints
-        for (int i = 0; i < randomInts.length; i++) {
-            randomInts[i] = rand.nextInt(15-1) + 1;; // storing random integers in an array
-            System.out.println(randomInts[i] + " "); // printing each array element
+        // initializes array of ints 1-15
+        for (int i = 0; i < randomInts.length - 1; i++) {
+            randomInts[i] = i + 1;
+        }
+
+        // randomly shuffles array of ints, leaves last element always 0
+        for (int i = 0; i < randomInts.length - 1; i++) {
+            int randomIndexToSwap = rand.nextInt(randomInts.length - 1);
+            int temp = randomInts[randomIndexToSwap];
+            randomInts[randomIndexToSwap] = randomInts[i];
+            randomInts[i] = temp;
+        }
+        return randomInts;
+    }
+
+    // 2D Array Representation of Board
+    public int[][] squaresArray() {
+        int [] randIntArr = randomizeNumbers(); // instance of random numbers
+        int [][] squaresArray = new int[4][4]; // new 2D array
+
+        // turns randomized number 1D array into a 2D array
+        for(int i = 0; i < squaresArray.length; i++) {
+            for (int j = 0; j < squaresArray[i].length; j++) {
+                squaresArray[i][j] = randIntArr[(i * squaresArray[i].length) + j];
+                System.out.println(squaresArray[i][j]);
+            }
+        }
+        return squaresArray;
+    }
+
+    // Method that determines if a square movement is a valid move
+    public void isValid (Button button, Button space) {
+        if (model.isButtonPressed) {
+//            if (button == findViewById()) {
+//
+//            }
         }
     }
 
